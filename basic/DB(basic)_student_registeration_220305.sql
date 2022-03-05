66908837e132aed685176f92b14ac5c13f4ae6b7
silversoren <ehsoren9@gmail.com>
2022-03-05 23:37:16 +0900

    Create DB(basic)_student_registeration_220305.sql

---
+++ basic/DB(basic)_student_registeration_220305.sql
@@ -0,0 +1,28 @@
+SELECT * FROM test.registration;
+
+select * from test.student;
+insert into test.student (studentnumber, studentname, major) values (100, '홍길동', '전기');
+
+
+
+
+
+
+-- ff
+
+
+
+
+insert into test.student values (200, '이순신', 4, '기계');
+select * from test.student;
+
+
+
+
+
+
+
+
+
+
+
